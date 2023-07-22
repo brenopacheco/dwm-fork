42be2613c4ca5391b2e8ec0cb0fba439cedcd89d
Breno Pacheco <brenoleonhardt@gmail.com>
2023-07-22 11:10:56 +0100

    adjustements

--- dwm.c
+++ dwm.c
@@ -371,12 +371,12 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "󰨑",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",    monocle },
 };
 
-static const char *monocles[] = { "󰎤", "󰼐", "󰼑", "󰼒", "󰼓", "󰼔", "󰼕", "󰼖", "󰼗", "󰼘" };
+static const char *monocles[] = { "󰼏", "󰼐", "󰼑", "󰼒", "󰼓", "󰼔", "󰼕", "󰼖", "󰼗", "󰼘" };
 
 /* key definitions */
 #define MODKEY Mod1Mask
